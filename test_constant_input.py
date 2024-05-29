3afb5090f255f3272ae69f225a6e24e3ea6152cf
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2024-05-29 12:18:51 +1000

    Base commit

---
+++ test_constant_input.py
@@ -0,0 +1,9 @@
+from pynput import keyboard
+
+print('Press s or n to continue:')
+
+with keyboard.Events() as events:
+    # Block for as much as possible
+    event = events.get(1e6)
+    if event.key == keyboard.KeyCode.from_char('s'):
+        print("YES")
