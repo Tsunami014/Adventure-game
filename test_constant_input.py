6c7cb687d84ffb5647e351333eb11a165df8f71c
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2024-05-29 12:30:19 +1000

    Made it a class

--- test_constant_input.py
+++ test_constant_input.py
@@ -1,9 +1,33 @@
 from pynput import keyboard
 
-print('Press s or n to continue:')
+class EventLoop:
+    def __init__(self):
+        self.pressing = []
+    
+    def check(self):
+        ks = []
+        with keyboard.Events() as events:
+            # Block for as much as possible
+            event = events.get(0.1)
+            if event is None:
+                return []
+            #if event.key == keyboard.KeyCode.from_char('s'):
+            if isinstance(event, keyboard.Events.Press):
+                if not event.key in self.pressing:
+                    if 'char' in dir(event.key):
+                        ks.append(event.key.char)
+                    else:
+                        ks.append(event.key.name)
+                    self.pressing.append(event.key)
+            elif isinstance(event, keyboard.Events.Release):
+                if event.key in self.pressing:
+                    self.pressing.remove(event.key)
+        return ks
 
-with keyboard.Events() as events:
-    # Block for as much as possible
-    event = events.get(1e6)
-    if event.key == keyboard.KeyCode.from_char('s'):
-        print("YES")
+print('Press s')
+e = EventLoop()
+
+while True:
+    found = e.check()
+    if found != []:
+        print(', '.join(found))
