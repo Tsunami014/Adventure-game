18431b9ebb86785c3df8b2706e09934eba53da60
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2024-06-12 12:00:24 +1000

    Rewrote movement and made chests work!
    Rewrote what happens when you move to allow the chest to stay instead of turn into a 'been there' tile
    Also improved the findnewsquares to have an optional distance of the search
    Which I use now the chests give you a torch to make you see further!

--- adventure_game_main.py
+++ adventure_game_main.py
@@ -10,22 +10,24 @@ playerHealth = 100 # eate a variable to store the player's health
 playerAttack = 10 # create a variable to store the player's attack power
 
 #----------------------------------------- SET UP THE GAME BOARD -----------------------------------------
-#create a 5x5 game board. 0 = empty, 1 = player, 2 = enemy, 3 = treasure, 4 = trap, 5 = exit, 6 = boss, 7 = been there, 8 = UnImplemented 9 = wall
+# Create a 5x5 game board. 0 = empty, 1 = player, 2 = enemy, 3 = treasure, 4 = trap, 5 = exit, 6 = boss, 7 = been there, 8 = chest, 9 = wall
+# Anything beyond 10 will not be put directly onto the board, but rather will be put there as an effect; (If need another base tile move 'been there' to a higher value)
+# 10 = open chest
 gameBoard = [[0,3,0,0,9,0,0,0,0,3,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0],
              [0,0,1,0,9,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
              [0,0,0,0,9,0,0,3,0,0,4,0,0,0,0,0,0,0,0,0,0,0,5,0,0],
-             [0,9,9,9,9,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0,0,4,4,4,4,4,4,4,4,4,4,4,4,4,4,0,0,0]]
+             [0,9,9,9,9,0,8,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
 
-# ! Means not implemented
-chars = [" ", "Y", "E", "$", "^", "/", "B", ":", "!", "#", "?"] # Characters that correlate with the numbers with "?" at the end
-ColourChars = [" ", "\033[96mY", "\033[32mE", "\033[93m$", "\033[32m^", "\033[94m/", "\033[31mB", "\033[90m:", "\033[91m!", "\033[97m#", "\033[90m?"] # The same as above but with COLOURS
+chars = [" ", "Y", "E", "$", "^", "/", "B", ":", "O", "#", "U", "?"] # Characters that correlate with the numbers with "?" at the end
+ColourChars = [" ", "\033[96mY", "\033[32mE", "\033[93m$", "\033[32m^", "\033[94m/", "\033[31mB", "\033[90m:", "\033[93mO", "\033[97m#", "\033[38;5;52mU", "\033[90m?"] # The same as above but with COLOURS
 
 foundBoard = [] # The board that you have discovered
 
 # Find the position of the player in the board and set the players' position to that
 playerY = [any([j == 1 for j in i]) for i in gameBoard].index(True)
 playerX = gameBoard[playerY].index(1)
+gameBoard[playerY][playerX] = 7
 
 # place the exit in a random location that does not already have something on it
 # Commented out to have a fixed exit
@@ -39,24 +41,30 @@ playerX = gameBoard[playerY].index(1)
 # ----------------------------------------- SET UP NON-CONSTANT VARIABLES -----------------------------------------
 toprints = [] # Setup the blank list of things that will be printed
 ca_chingInTheBank = 0 # Player moneys
+inventory = {"light": False} # The player's inventory; with nothing in it.
 
 # ----------------------------------------- SET UP THE FUNCTIONS -----------------------------------------
 # create the def() functions for the program here
 def printBoard():
     stats = f"Player moneys: {ca_chingInTheBank}\nPlayer health: {playerHealth}HP"
-    print('\033[2J\033[0;0H' + '\n'.join([''.join([(ColourChars[gameBoard[i][j]] if (i, j) in foundBoard else ColourChars[-1]) for j in range(len(gameBoard[i]))]) for i in range(len(gameBoard))]) + '\n\033[35m' + stats + '\033[0m\n\n' + '\n'.join(toprints), end='')
+    print('\033[2J\033[0;0H' + '\n'.join([''.join([(ColourChars[1] if i == playerY and j == playerX else (ColourChars[gameBoard[i][j]] if (i, j) in foundBoard else ColourChars[-1])) for j in range(len(gameBoard[i]))]) for i in range(len(gameBoard))]) + '\n\033[35m' + stats + '\033[0m\n\n' + '\n'.join(toprints), end='')
 
 def newprint(txt):
     toprints.append(str(txt))
 
-def findNewSquares():
-    playerPos = [playerY, playerX]
-    def check(pos):
-        if tuple(pos) not in foundBoard:
-            foundBoard.append(tuple(pos))
+def findNewSquares(pos=None, dist=1):
+    if pos is None:
+        pos = [playerY, playerX]
+    def check(p):
+        if dist > 1:
+            findNewSquares(p, dist - 1)
+        else:
+            if tuple(p) not in foundBoard:
+                foundBoard.append(tuple(p))
     def md(x, y): # MoDify
-        return [playerPos[0] + y, playerPos[1] + x]
-    check(playerPos)
+        return [pos[0] + y, pos[1] + x]
+    if tuple(pos) not in foundBoard:
+        foundBoard.append(tuple(pos))
     check(md(1, 0))
     check(md(1, 1))
     check(md(0, 1))
@@ -77,14 +85,24 @@ def get_hurt(minusHP):
     if playerHealth <= 0:
         EndGame(False, "You've run out of life!")
 
-def movedOn(typ):
-    if typ == 5:
+def movedOn(typ): # You moved onto a tile
+    # Once-off tiles
+    if typ == 5: # Exit
         EndGame(True, 'You have reached the exit!')
-    elif typ == 3:
+    elif typ == 3: # Moneys
         global ca_chingInTheBank
         ca_chingInTheBank += 1
-    elif typ == 4:
+    elif typ == 4: # Trap
         get_hurt(10)
+    
+    # Tiles that change
+    if typ == 8: # Chest
+        global inventory
+        inventory["light"] = True
+        return 10
+
+    # And if it isn't a tile that changes above, turn it into a blank
+    return 7
 
 def moveBy(byx, byy):
     global playerX, playerY
@@ -96,10 +114,9 @@ def moveBy(byx, byy):
     if newPos == 9:
         newprint('You cannot move there - there is a wall in the way!')
         return
-    gameBoard[playerY][playerX] = 7
     playerX, playerY = x, y
-    movedOn(newPos)
-    gameBoard[playerY][playerX] = 1
+    res = movedOn(newPos)
+    gameBoard[playerY][playerX] = res
 
 def movePlayer(direction):
     direction = direction.lower()
@@ -111,7 +128,7 @@ def movePlayer(direction):
         moveBy(1, 0)
     elif direction == 'a':
         moveBy(-1, 0)
-    findNewSquares()
+    findNewSquares(dist=(2 if inventory["light"] else 1))
 
 def onpress(key):
     global toprints
